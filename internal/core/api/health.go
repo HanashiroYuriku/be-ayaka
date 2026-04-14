fdd4e00b64fa517afac30230ff929381182c2797
HanashiroYuriku <devan051003@gmail.com>
2026-04-14 07:06:26 +0700

    add: add database healt check

--- internal/core/api/health.go
+++ internal/core/api/health.go
@@ -3,29 +3,45 @@ package api
 import (
 	"be-ayaka/config"
 	ayaka "be-ayaka/pkg/logger"
-	"be-ayaka/pkg/response"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
 
 type HealthCheckHandler struct {
 	Config *config.Config
+	db *gorm.DB
 }
 
-func NewHealthCheckHandler(cfg *config.Config) *HealthCheckHandler {
+func NewHealthCheckHandler(cfg *config.Config, db *gorm.DB) *HealthCheckHandler {
 	return &HealthCheckHandler{
 		Config: cfg,
+		db: db,
 	}
 }
 
-func (h *HealthCheckHandler) Ping(c *fiber.Ctx) error {
+func (h *HealthCheckHandler) Check(c *fiber.Ctx) error {
 	go ayaka.Log("SYSTEM", "INFO", "Ayaka Server is running")
+	appStatus := "UP"
+	dbStatus := "UP"
+	httpCode := fiber.StatusOK
 
-	data := fiber.Map{
-		"serverName": h.Config.App.Name,
-		"version":     h.Config.App.Version,
-		"isHealthy":  true,
+	sqlDB, err := h.db.DB() 
+	
+	if err != nil || sqlDB.Ping() != nil {
+		appStatus = "DOWN"
+		dbStatus = "DOWN"
+		httpCode = fiber.StatusServiceUnavailable
 	}
 
-	return c.Status(fiber.StatusOK).JSON(response.NewSuccessResponse(data))
+	return c.Status(httpCode).JSON(fiber.Map{
+		"name":    h.Config.App.Name,
+		"status":  appStatus,
+		"time":    time.Now().Format(time.RFC3339),
+		"components": fiber.Map{
+			"server":   "UP",       
+			"database": dbStatus,   
+		},
+	})
 }

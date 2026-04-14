f72fd585b5e2dfefdbc0783f528eb18965c9d0ab
HanashiroYuriku <devan051003@gmail.com>
2026-04-14 22:46:23 +0700

    refract: move to bootstrap

--- internal/bootstrap/builder.go
+++ internal/bootstrap/builder.go
@@ -1,18 +1,18 @@
-package core
+package bootstrap
 
 import (
 	adapterRepo "be-ayaka/internal/adapter/repository"
-	"be-ayaka/internal/core/api"
+	"be-ayaka/internal/delivery/http"
 	"be-ayaka/internal/core/service"
 
 	"gorm.io/gorm"
 )
 
-func BuildUserHandler(db *gorm.DB) *api.UserHandler {
+func BuildUserHandler(db *gorm.DB) *http.UserHandler {
 	// adapter
 	userRepo := adapterRepo.NewUserRepoPostgres(db)
 	// service
 	userService := service.NewUserService(userRepo)
 	// handler
-	return api.NewUserHandler(userService)
+	return http.NewUserHandler(userService)
 }

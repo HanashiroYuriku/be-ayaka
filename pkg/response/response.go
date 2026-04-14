0e51c5a0b03619aa1b10ff8c245150f62e366d1d
HanashiroYuriku <devan051003@gmail.com>
2026-04-14 07:07:46 +0700

    add: add forbidden status

--- pkg/response/response.go
+++ pkg/response/response.go
@@ -9,6 +9,7 @@ const (
 	InternalServerError Status = "SERVER_ERROR"
 	DataConflict        Status = "DATA_CONFLICT"
 	Unauthorized        Status = "UNAUTHORIZED"
+	Forbidden           Status = "FORBIDDEN"
 )
 
 func (s Status) String() string {
@@ -20,35 +21,37 @@ type Response struct {
 	Status           string      `json:"status"`
 	Message          string      `json:"message"`
 	Data             interface{} `json:"data,omitempty"`
-	TraceID          string      `json:"traceId,omitempty"`
+	RequestId        string      `json:"requestId,omitempty"`
 	StatusCodeClient *int        `json:"statusCodeClient,omitempty"`
 	ErrorList        interface{} `json:"errorList,omitempty"`
 	ErrorField       interface{} `json:"errorField,omitempty"`
 }
 
 // NewSuccessResponse creates a new success response with the given data
-func NewSuccessResponse(data interface{}) *Response {
+func NewSuccessResponse(status Status, message string, data interface{}, requestId string) *Response {
 	return &Response{
-		Status:  StatusSuccess.String(),
-		Message: "Request successful",
-		Data:    data,
+		Status:    status.String(),
+		Message:   message,
+		Data:      data,
+		RequestId: requestId,
 	}
 }
 
 // NewErrorResponse creates a new error response with the given message and optional error details
 func NewErrorResponse(status Status, message string, id string) *Response {
 	return &Response{
-		Status:  status.String(),
-		Message: message,
-		TraceID: id,
+		Status:    status.String(),
+		Message:   message,
+		RequestId: id,
 	}
 }
 
 // NewErrorFieldResponse creates a new error response with the given message and error field details
-func NewErrorFieldResponse(status Status, message string, err interface{}) *Response {
+func NewErrorFieldResponse(status Status, message string, err interface{}, id string) *Response {
 	return &Response{
 		Status:     status.String(),
 		Message:    message,
+		RequestId:  id,
 		ErrorField: ParseErrorField(err.(string)),
 	}
 }

d3e248108d1a3fa0f5ceabc14a72bf5d5a6d88ee
HanashiroYuriku <devan051003@gmail.com>
2026-05-19 10:14:34 +0700

    feat: implement GenerateID function for UUID generation with optional prefix

---
+++ pkg/generateid/id.go
@@ -0,0 +1,18 @@
+package generateid
+
+import (
+	"strings"
+
+	"github.com/google/uuid"
+)
+
+func GenerateID(prefix string) string {
+	newUUID := uuid.New().String()
+
+	cleanUUID := strings.ReplaceAll(newUUID, "-", "")
+
+	if prefix != "" {
+		return strings.ToUpper(prefix) + "-" + cleanUUID
+	}
+	return "ID-" + cleanUUID
+}

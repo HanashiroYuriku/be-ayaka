a07fd133519bc1d1b7fc8663afd3415b96378995
HanashiroYuriku <devan051003@gmail.com>
2026-05-19 13:31:07 +0700

    refactor: update user repository methods to accept context and improve error handling

--- internal/adapter/repository/user_repo.go
+++ internal/adapter/repository/user_repo.go
@@ -1,8 +1,10 @@
 package repository
 
 import (
+	"be-ayaka/internal/core/customerrors"
 	"be-ayaka/internal/core/entity"
 	"be-ayaka/internal/core/port"
+	"context"
 	"errors"
 
 	"gorm.io/gorm"
@@ -12,36 +14,37 @@ type userRepoPostgres struct {
 	db *gorm.DB
 }
 
-func NewUserRepoPostgres(db *gorm.DB) port.UserRepository {
+func NewUserRepo(db *gorm.DB) port.UserRepository {
 	return &userRepoPostgres{
 		db: db,
 	}
 }
 
-func (r *userRepoPostgres) FindByID(id string) (*entity.User, error) {
+func (r *userRepoPostgres) FindByID(ctx context.Context, id string) (*entity.User, error) {
 	var user entity.User
-	err := r.db.First(&user, "id = ?", id).Error
+	err := r.db.WithContext(ctx).First(&user, "id = ?", id).Error
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return nil, nil
+			return nil, customerrors.ErrDataNotFound
 		}
 		return nil, err
 	}
 	return &user, nil
 }
 
-func (r *userRepoPostgres) FindByEmail(email string) (*entity.User, error) {
+func (r *userRepoPostgres) FindByEmail(ctx context.Context, email string) (*entity.User, error) {
 	var user entity.User
-	err := r.db.Where("email = ?" , email).First(&user).Error
+	err := r.db.WithContext(ctx).Where("email = ?", email).First(&user).Error
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return nil, nil
+			return nil, customerrors.ErrInvalidCredentials
 		}
 		return nil, err
 	}
 	return &user, nil
 }
 
-func (r *userRepoPostgres) Create(user *entity.User) error {
-	return r.db.Create(user).Error
+func (r *userRepoPostgres) Create(ctx context.Context, user *entity.User) error {
+	db := ExtractTx(ctx, r.db)
+	return db.WithContext(ctx).Create(user).Error
 }

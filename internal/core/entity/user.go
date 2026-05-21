cf64d639cc67352edbbd065fe12e61d90d8732a4
HanashiroYuriku <devan051003@gmail.com>
2026-05-21 22:11:45 +0700

    fix: update User struct fields to include GORM tags for validation and constraints

--- internal/core/entity/user.go
+++ internal/core/entity/user.go
@@ -2,8 +2,8 @@ package entity
 
 type User struct {
 	BaseEntity
-	Username string `json:"username"`
-	Email    string `json:"email" gorm:"unique"`
-	Password string `json:"-"`
-	Role     string `json:"role"`
+	Username string `gorm:"unique;not null;type:varchar(20)"`
+	Email    string `gorm:"unique;not null;type:varchar(100)"`
+	Password string `gorm:"not null;type:text"`
+	Role     string `gorm:"type:varchar(20);default:'user';check:role IN ('admin', 'user')"`
 }

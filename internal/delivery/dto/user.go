6fcc276c9671d289ba843599c018da58db931285
HanashiroYuriku <devan051003@gmail.com>
2026-05-21 22:11:26 +0700

    refactor: move UserRequest and UserResponse DTOs for user management from entity to dto

--- internal/delivery/dto/user.go
+++ internal/delivery/dto/user.go
@@ -1,8 +1,16 @@
-package entity
+package dto
 
 type UserRequest struct {
 	Username    string `json:"username" validate:"required,min=3,max=20,unique=users->username,whitespace,username" example:"hanashiroyuriku"`
 	Email       string `json:"email" validate:"required,max=100,email,unique=users->email,whitespace" example:"hanashiroyuriku@gmail.com"`
 	DisplayName string `json:"displayName" example:"Hanashiro Yuriku"`
 	Password    string `json:"password" validate:"required,complexpassword" example:"P4$$word"`
+}
+
+type UserResponse struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email" gorm:"unique"`
+	Password string `json:"-"`
+	Role     string `json:"role"`
 }

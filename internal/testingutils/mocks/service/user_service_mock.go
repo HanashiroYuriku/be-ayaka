1adff18c6fdc48c06a1c4dabd16ced2d3098c9a4
HanashiroYuriku <devan051003@gmail.com>
2026-05-21 22:12:15 +0700

    refactor: update MockUserService to use DTOs for user requests and responses

--- internal/testingutils/mocks/service/user_service_mock.go
+++ internal/testingutils/mocks/service/user_service_mock.go
@@ -1,7 +1,7 @@
 package mocks
 
 import (
-	"be-ayaka/internal/core/entity"
+	"be-ayaka/internal/delivery/dto"
 	"context"
 
 	"github.com/stretchr/testify/mock"
@@ -11,15 +11,15 @@ type MockUserService struct {
 	mock.Mock
 }
 
-func (m *MockUserService) Create(ctx context.Context, user *entity.UserRequest) error {
+func (m *MockUserService) Create(ctx context.Context, user *dto.UserRequest) error {
 	args := m.Called(ctx, user)
 	return args.Error(0)
 }
 
-func (m *MockUserService) GetProfile(ctx context.Context, userID string) (*entity.User, error) {
+func (m *MockUserService) GetProfile(ctx context.Context, userID string) (*dto.UserResponse, error) {
 	args := m.Called(ctx, userID)
 	if args.Get(0) != nil {
-		return args.Get(0).(*entity.User), args.Error(1)
+		return args.Get(0).(*dto.UserResponse), args.Error(1)
 	}
 	return nil, args.Error(1)
 }
